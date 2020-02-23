4fe7be472f22423d8f14f1c9a3869b228eb9f9c8
Faris Ali <faris.assir@gmail.com>
2020-02-23 15:38:48 +0100

    fixing Main menu and test

--- src/com/HolidayMaker/MainMenu.java
+++ src/com/HolidayMaker/MainMenu.java
@@ -11,15 +11,31 @@ public class MainMenu {
 
     private void menu(){
         while (true) {
-            System.out.println("        Holidaymaker        ");
-            System.out.println("2. Register a customer");
-            System.out.println("3. Show available room within specified period");
-            System.out.println("4. Reserve a room");
-            System.out.println("5. add Extra bed");
-            System.out.println("6. Cancel booking");
+            System.out.println("       Welcome to Holidaymaker Management System        ");
+            System.out.println("                        Main Menu                       ");
+            System.out.println("                1. Register a customer");
+            System.out.println("                2. Show available room within specified period");
+            System.out.println("                3. Reserve a room");
+            System.out.println("                4. add Extra bed");
+            System.out.println("                5. Cancel booking");
             String option= input.nextLine();
 
-
+            switch (option){
+                case"1":
+                    System.out.println("register");
+                    break;
+                case "2":
+                    System.out.println("availability");
+                    break;
+                case "3":
+                    System.out.println("booking");
+                    break;
+                case "4":
+                    System.out.println("Extra bed");
+                    break;
+                case "5":
+                    System.out.println("cancellation");
+            }
         }
     }
 }
