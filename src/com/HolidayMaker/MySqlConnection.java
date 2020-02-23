9f78a88e6d4badbcec968b0abd3c15e29b3ecca1
Faris Ali <faris.assir@gmail.com>
2020-02-23 15:00:30 +0100

    create the connection method

--- src/com/HolidayMaker/MySqlConnection.java
+++ src/com/HolidayMaker/MySqlConnection.java
@@ -1,5 +1,27 @@
 package com.HolidayMaker;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class MySqlConnection {
-    
+    private Connection conn = null;
+    private PreparedStatement statement;
+    private ResultSet resultSet;
+
+public MySqlConnection(){
+    connect();
 }
+private void connect(){
+    try{
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/holidayMaker?user=root&password=Ics@00Fine&serverTimezone=UTC");
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+}
+
+}
+
+
+
