4ee25fd29bad6c3884e3f8553e8026990a2c35c2
Faris Ali <faris.assir@gmail.com>
2020-02-23 15:17:22 +0100

    Add the main menu options

--- src/com/HolidayMaker/Main.java
+++ src/com/HolidayMaker/Main.java
@@ -3,6 +3,7 @@ package com.HolidayMaker;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	MainMenu mainMenu = new MainMenu();
+	mainMenu.start();
     }
 }
--- src/com/HolidayMaker/MainMenu.java
+++ src/com/HolidayMaker/MainMenu.java
@@ -1,4 +1,33 @@
 package com.HolidayMaker;
 
+import java.util.Scanner;
+
 public class MainMenu {
+    Scanner input;
+
+    public MainMenu(Scanner input) {
+        this.input=new Scanner(System.in);
+    }
+
+    public MainMenu() {
+
+    }
+
+    public void start() {
+        this.menu();
+    }
+
+    private void menu() {
+        while (true) {
+            System.out.println("        Holidaymaker        ");
+            System.out.println("2. Register a customer");
+            System.out.println("3. Show available room within specified period");
+            System.out.println("4. Reserve a room");
+            System.out.println("5. add Extra bed");
+            System.out.println("6. Cancel booking");
+            String option=this.input.nextLine();
+
+
+        }
+    }
 }
